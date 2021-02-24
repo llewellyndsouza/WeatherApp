78f242e4cb710b101ef3cd0fa403855a664db6c1
Llewellyn Dsouza <lledsouza2209@gmail.com>
2021-02-24 19:17:01 +0530

    Working display

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import getWeatherData from "./weather.js";
 
 (() => {
+  // DOM cache
+  let cityTextInput = document.querySelector("#cityTextInput");
+  let searchCityButton = document.querySelector("#searchCityButton");
+  let tempDisplay = document.querySelector(".tempDisplay");
+  let feelsLikeDisplay = document.querySelector(".feelsLikeDisplay");
+  let descriptionDisplay = document.querySelector(".descriptionDisplay");
+  let locationDisplay = document.querySelector(".locationDisplay");
 
-  getWeatherData("London")
-    .then((data) => {
-      console.log(data);
-    })
-    .catch(console.log);
+  //Set triggers
+  searchCityButton.addEventListener("click", refreshTemp);
+
+  //Functions
+
+  //Refreshes the temperature
+  async function refreshTemp(city) {
+    if (cityTextInput.value == "") cityTextInput.value = "Mumbai";
+
+    getWeatherData(cityTextInput.value)
+      .then((data) => {
+        cityTextInput.value = data.name;
+        updateDisplay(data);
+      })
+      .catch(console.log);
+  }
+
+  function updateDisplay(data) {
+    locationDisplay.innerText = data.name + ", " + data.sys.country;
+    tempDisplay.innerText = data.main.temp-273.15;
+    feelsLikeDisplay.innerText = data.main.feels_like-273.15;
+    descriptionDisplay.innerText = data.weather[0].description;
+  }
+
+  //Initialization
+  refreshTemp("Mumbai");
 })();
